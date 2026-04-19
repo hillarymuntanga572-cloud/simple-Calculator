9cc6203222429cbc3d5ee76c59914b2d3ae71b8d
Muntanga <hillarymuntanga572@gmail.com>
2026-04-19 13:24:07 +0200

    updates

--- calculator/index.js
+++ calculator/index.js
@@ -1,3 +1,9 @@
 const display=getElementById(displayBtn);
 document.getElementById("eightBtn").onclick = function(){ document.getElementById(displayBtn).textContent ="8"
 }
+function animateDisplay() {
+    display.style.transform = "scale(1.05)";
+    setTimeout(() => {
+        display.style.transform = "scale(1)";
+    }, 100);
+}
